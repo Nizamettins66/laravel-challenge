5ea6463dc856f23fa65c2af03431c424f32bfa9e
nizamettin sari <nizamettin.sari@student.rocva.nl>
2024-12-10 14:04:09 +0100

    Extra Euro-teken en totaal

--- Woning/resources/views/woning/index.blade.php
+++ Woning/resources/views/woning/index.blade.php
@@ -1,6 +1,7 @@
 @section('title', 'List of Woningen')
 @extends('woning/base')
 @section('content')
+@php( $sum=0 )
   <div>
     <a href="/woning/create" class="btn btn-primary mb-3">Add Woning</a>
   </div>     
@@ -19,11 +20,12 @@
     </thead>
     <tbody>
         @foreach($woning as $w)
+        @php( $sum+=$w->prijsperweek )
         <tr>
             <td>{{$w->id}}</td>
             <td>{{$w->titel}} </td>
             <td>{{$w->oppervlakte}}</td>
-            <td>{{$w->prijsperweek}}</td>
+            <td>€{{ number_format($w->prijsperweek, 2, ',', '.')}}</td>
             <td>{{$w->updated_at}}</td>
             <td><a href="/woning/edit/{{$w->id}}" class="btn btn-primary">Edit</a></td>
             <td>
@@ -34,6 +36,13 @@
           </td>
         </tr>
         @endforeach
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>€{{ number_format($sum, 2, ',', '.')}}</td>
+        <td></td>
+        <td></td>
+        <td></td>
     </tbody>
   </table>
 @endsection
