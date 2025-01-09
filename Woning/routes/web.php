3e6dbe51aaf95d2e669a8cd0f28c7122caaf8193
nizamettin sari <nizamettin.sari@student.rocva.nl>
2025-01-09 11:36:55 +0100

    Login first step

--- Woning/routes/web.php
+++ Woning/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,3 +18,15 @@ Route::get('woning/edit/{id}',   [App\Http\Controllers\WoningController::class, 
 Route::post('woning/update/{id}',   [App\Http\Controllers\WoningController::class, 'update']);
 
 Route::post('woning/destroy/{id}',  [App\Http\Controllers\WoningController::class, 'destroy']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
