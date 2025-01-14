ee31eb60d41ac026387c7c3dd64dcd4494c5d284
nizamettin sari <nizamettin.sari@student.rocva.nl>
2025-01-14 10:07:55 +0100

    Back up laravel

--- Woning/resources/views/woning/create.blade.php
+++ Woning/resources/views/woning/create.blade.php
@@ -1,17 +1,6 @@
+@section('title', 'Nieuwe Woningen')
 @extends('woning/base')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Woning</title>
-</head>
-<body>
-  <div class="container" style="margin:40px;">
-
-  <h1 class="display-3"> Nieuwe Woningen</h1>
+@section('content')
 
     <form method="post" action="/woning/store">
           @csrf
@@ -31,8 +20,4 @@
           </div>
           <button type="submit" class="btn btn-primary">Add woning</button>
       </form>
-
-  </div>
-
-</body>
-</html>
+@endsection
--- Woning/routes/web.php
+++ Woning/routes/web.php
@@ -6,7 +6,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('woning/store', [App\Http\Controllers\WoningController::class, 'store']);
+
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('woning/store', [App\Http\Controllers\WoningController::class, 'store']);
 
 
 Route::get('woning', [App\Http\Controllers\WoningController::class, 'index']);
@@ -18,15 +33,6 @@ Route::get('woning/edit/{id}',   [App\Http\Controllers\WoningController::class, 
 Route::post('woning/update/{id}',   [App\Http\Controllers\WoningController::class, 'update']);
 
 Route::post('woning/destroy/{id}',  [App\Http\Controllers\WoningController::class, 'destroy']);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
